e11fe42187d817b622a8255f1053ca73b989202f
LeeGyeongjinny <yikj555@gmail.com>
2025-04-24 23:19:05 +0900

    250424 이경진 코멘트아웃 수정

--- sayclub/src/Lib/Auth.php
+++ sayclub/src/Lib/Auth.php
@@ -6,24 +6,24 @@ class Auth {
     
     
     public static function chkAuthorization () {
-        // //비로그인시 접속 불가능한 리스트
-        // $arrNeedAuth = [
-        //     'main' // 친구 목록
-        //     ,'registration' // 회원가입
-        //     ,'logout'
-        //     ,'diary'
-        // ];
-        // $url = $_GET['url'];
+        //비로그인시 접속 불가능한 리스트
+        $arrNeedAuth = [
+            'main' // 친구 목록
+            ,'registration' // 회원가입
+            ,'logout'
+            ,'diary'
+        ];
+        $url = $_GET['url'];
 
-        // //접속 권한이 없는 페이지 접속 차단(로그인 페이지로 이동)
-        // if(!isset($_SESSION['user_account']) && in_array($url, $arrNeedAuth)) {
-        //     header('Location: /login'); //로그인이 안되어 있을때 로그인창으로 이동
-        //     exit;
-        // }
+        //접속 권한이 없는 페이지 접속 차단(로그인 페이지로 이동)
+        if(!isset($_SESSION['user_account']) && in_array($url, $arrNeedAuth)) {
+            header('Location: /login'); //로그인이 안되어 있을때 로그인창으로 이동
+            exit;
+        }
 
-        // if(isset($_SESSION['user_account']) && !in_array($url, $arrNeedAuth)) {
-        //     header('Location: /main');
-        //     exit;
-        // }
+        if(isset($_SESSION['user_account']) && !in_array($url, $arrNeedAuth)) {
+            header('Location: /main');
+            exit;
+        }
     }
 }
